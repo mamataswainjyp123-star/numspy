bf10fadeeb432deab22f9bd37616bcc876992e44
mamataswainjyp123-star <mamataswainjyp123@gmail.com>
2026-08-28 09:46:53 +0530

    Update credentials.py

--- Examples/credentials.py
+++ Examples/credentials.py
@@ -1,3 +1,3 @@
-username = 'your way2sm username'
-password = 'your way2sms password'
-mobile_number = 'receivers mobile number'
+username = 'your way2sm username' world_of_drapes
+password = 'your way2sms password' 123
+mobile_number = 'receivers mobile number' 7799088200
